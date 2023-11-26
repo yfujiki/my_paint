6c5a69054ce94be0c94e744e9d301a30b24f90c8
Yuichi Fujiki <fjk89025@gmail.com>
2023-11-26 15:10:14 +0900

    Save painted image into file

--- lib/drawing_area.dart
+++ lib/drawing_area.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'drawing_point.dart';
 import 'my_custom_painter.dart';
@@ -11,6 +17,7 @@ class DrawingArea extends StatefulWidget {
 }
 
 class DrawingAreaState extends State<DrawingArea> {
+  final GlobalKey _globalKey = GlobalKey();
   List<DrawingPoint?> points = [];
   Paint paint = Paint()
     ..color = Colors.black
@@ -20,24 +27,70 @@ class DrawingAreaState extends State<DrawingArea> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: (details) {
-        setState(() {
-          RenderBox renderBox = context.findRenderObject() as RenderBox;
-          points.add(DrawingPoint(
-              position: renderBox.globalToLocal(details.globalPosition),
-              paint: paint));
-        });
-      },
-      onPanEnd: (details) {
-        setState(() {
-          points.add(null);
-        });
-      },
-      child: CustomPaint(
-        painter: MyCustomPainter(points: points),
-        child: Container(),
-      ),
+    return Stack(
+      children: [
+        RepaintBoundary(
+          key: _globalKey,
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              setState(() {
+                RenderBox renderBox = context.findRenderObject() as RenderBox;
+                points.add(DrawingPoint(
+                    position: renderBox.globalToLocal(details.globalPosition),
+                    paint: paint));
+              });
+            },
+            onPanEnd: (details) {
+              setState(() {
+                points.add(null);
+              });
+            },
+            child: CustomPaint(
+              painter: MyCustomPainter(points: points),
+              child: Container(),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 10,
+          right: 10,
+          child: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                points.clear();
+              });
+            },
+            child: const Icon(Icons.clear),
+          ),
+        ),
+        Positioned(
+          top: 10,
+          left: 10,
+          child: FloatingActionButton(
+            onPressed: () {
+              saveToImage();
+            },
+            child: const Icon(Icons.save),
+          ),
+        ),
+      ],
     );
+  }
+
+  Future<void> saveToImage() async {
+    RenderRepaintBoundary? boundary =
+        _globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
+    if (boundary == null) return;
+    final image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+    if (byteData == null) return;
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+    await saveImage(pngBytes);
+  }
+
+  Future<void> saveImage(Uint8List pngBytes) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final imagePath = await File('${directory.path}/my_image.png').create();
+    await imagePath.writeAsBytes(pngBytes);
   }
 }
